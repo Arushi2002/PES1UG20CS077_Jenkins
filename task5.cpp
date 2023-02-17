4f132dcf64821312a77dccf76ef2414fcbb65423
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2023-02-17 09:55:44 +0530

    Create task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hi!\n";
+  cout<< "We have successfully created a new pipeline!\n";
+  cout<< "This is task 5\n";
+  return 0;
+}
